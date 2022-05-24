4a4d55bb4e24f9ca4ea455ece9652ea28a138ccc
Alex Valdez <xvaldez0411@gmail.com>
2022-05-23 23:49:28 -0500

    finished assignment

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("connected to js");
+
+function changeColor(element) {
+    element.style.color = "aquamarine";
+}
+
+function changeBack(element) {
+    element.style.color = "black";
+}
